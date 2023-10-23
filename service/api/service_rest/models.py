4cbd1e2b83eb3a037455fb1705b581f802470e2a
Erin <ahnys95@yahoo.com>
2023-10-23 14:57:59 -0500

    admin and models

--- service/api/service_rest/models.py
+++ service/api/service_rest/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+
+class AutomobileVO(models.Model):
+    vin = models.CharField(max_length=200)
+    sold = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"VIN: {self.vin}"
+
+
+class Technician(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    employee_id = models.CharField(max_length=200, unique=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.lastname}"
+
+
+class Appointment(models.Model):
+    date_time = models.DateTimeField()
+    reason = models.CharField(max_length=200)
+    status = models.CharField(max_length=200)
+    vin = models.ForeignKey(
+        AutomobileVO,
+        related_name="appointments",
+        on_delete=models.CASCADE
+    )
+    customer = models.CharField(max_length=200)
+    technician = models.ForeignKey(
+        Technician,
+        related_name="appointments",
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.date_time
+
+    def get_api_url(self):
+        return reverse("api_appointment", kwargs={"pk": self.pk})
