01d43f52430c4162030ee05b983c678f41a7b479
Daniel Glasgow <danielrossglasgow@gmail.com>
2023-10-24 11:42:28 -0500

    sales backend complete

--- sales/api/sales_rest/models.py
+++ sales/api/sales_rest/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class AutomobileVO(models.Model):
+    vin = models.CharField(max_length=200)
+    sold = models.BooleanField(default=False)
+
+
+class Salesperson(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    employee_id = models.CharField(max_length=200)
+
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    phone_number = models.CharField(max_length=200)
+
+class Sale(models.Model):
+    price = models.CharField(max_length=200)
+    automobile = models.ForeignKey(
+        AutomobileVO,
+        related_name="sales",
+        on_delete=models.CASCADE
+    )
+    salesperson = models.ForeignKey(
+        Salesperson,
+        related_name="sales",
+        on_delete=models.CASCADE
+    )
+    customer = models.ForeignKey(
+        Customer,
+        related_name="sales",
+        on_delete=models.CASCADE
+    )
+
+    def get_api_url(self):
+        return reverse("api_vehicle_model", kwargs={"pk": self.id})
