ae3b8e38ce2eaacf62acc92bfe008c3aade1ed42
Erin <ahnys95@yahoo.com>
2023-12-16 17:03:37 -0600

    navbar

--- ghi/app/src/Nav.js
+++ ghi/app/src/Nav.js
@@ -1,10 +1,17 @@
 import { NavLink } from 'react-router-dom';
+import logoImage from "./imgs/logo.png";
 
 function Nav() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-success">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container-fluid">
-        <NavLink className="navbar-brand" to="/">CarCar</NavLink>
+        <NavLink className="navbar-brand" to="/"><img
+                            src={logoImage}
+                            alt="Logo"
+                            className="logo-image"
+                            style={{ width: '50px', height: '50px' }}
+                        />
+        </NavLink>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
